da6980ad26276a69bb8429e57087fdec0d89eaec
luolin <luolinae86@gmail.com>
2018-05-28 16:13:16 +0800

    增加发送短信的接口

--- lib/sms_voice_flash.rb
+++ lib/sms_voice_flash.rb
@@ -1,5 +1,21 @@
 require "sms_voice_flash/version"
+require 'rest-client'
+require 'json'
 
 module SmsVoiceFlash
-  # Your code goes here...
+
+  extend self
+
+  SmsUrl = "https://k.ylb.io/api/message/v0/send"
+
+  def set(key,sign)
+    @key = key
+    @sign = sign
+  end
+
+  def sms(mobile,content)
+    url = URI.encode("#{SmsUrl}?apikey=#{@key}&mobile=#{mobile}&content=#{content}&ts=#{Time.now.to_i}&sign=#{@sign}")
+    result = JSON.parse(RestClient.get(url).body,symbolize_names: true)
+  end
+
 end
