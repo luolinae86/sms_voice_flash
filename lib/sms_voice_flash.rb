a219a7a58a81a087ba3ff2d72ce855188a48ac0a
luolin <luolinae86@gmail.com>
2018-05-31 09:59:30 +0800

    支持批量发送短信

--- lib/sms_voice_flash.rb
+++ lib/sms_voice_flash.rb
@@ -16,7 +16,7 @@ module SmsVoiceFlash
   def sms(mobile,content)
     #如果用户输入的为数组，则将数组内的元素用逗号分隔成字符串
     if mobile.kind_of?(Array)
-      mobile.map!{|number| number.strip}.join(",")
+      mobile = mobile.map{|number| number.strip}.join(",")
     end
     url = URI.encode("#{SmsUrl}?apikey=#{@key}&mobile=#{mobile}&content=#{content}&ts=#{Time.now.to_i}&sign=#{@sign}")
     result = JSON.parse(RestClient.get(url).body,object_class: OpenStruct)
